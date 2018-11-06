b87fd8d3fb87147713b0dba7c87432b0fe21b880
buyuanlee <buyuanlee@gmail.com>
2018-11-06 22:29:47 +0800

    v-for遍历

---
+++ v-for/index.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: "#app",
+    data: {
+        todos: ['吃饭', '睡觉', '打豆豆']
+    },
+    // methods: {
+    //     toggle: function (event) {
+    //         event.done = !event.done
+    //     }
+    // }
+})
